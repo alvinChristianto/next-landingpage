0b4e0150743f6b4cc63d9a300a75faf6631b0212
Alvin Christianto <39409708+alvinChristianto@users.noreply.github.com>
2025-10-09 10:07:37 +0700

    Update APIhotel.tsx
    refactor: adding env variable for backend, api version, and hotel secret code

--- app/API/APIhotel.tsx
+++ app/API/APIhotel.tsx
@@ -1,19 +1,23 @@
 import Axios from 'axios';
 
 
+const HOTEL_SECRET = process.env.NEXT_HOTEL_SECRET_CODE;
+const BE_URL = process.env.NEXT_PUBLIC_BE_ROUTE;
+const VERS = process.env.NEXT_PUBLIC_API_VERS;
+
 async function hotel() {
     let response;
     let headers = {
         //  'Content-Type': 'application/json',
         Accept: "application/json",
-        Authorization_hotel: "test12345",
+        Authorization_hotel: HOTEL_SECRET,
         // 'Access-Control-Allow-Methods': "POST, PUT, PATCH, GET, DELETE, OPTIONS",
         "Access-Control-Allow-Origin": "*",
         // 'Access-Control-Allow-Headers': "Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization",
     };
 
     //console.log(BE_URL + VERS + 'blogs');
-    await Axios.get('http://127.0.0.1:8000/api/v1/hotel', {
+    await Axios.get(BE_URL + VERS + 'hotel', {
         headers,
         withCredentials: true,
     })
@@ -42,7 +46,7 @@ async function room_hotel() {
     };
 
     //console.log(BE_URL + VERS + 'blogs');
-    await Axios.get('http://127.0.0.1:8000/api/v1/room_type', {
+   await Axios.get(BE_URL + VERS + 'room_type', {
         headers,
         withCredentials: true,
     })
